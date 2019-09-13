929f529fe222ec03bd55d2992b1b4a1f5f4077c3
kuntalghosh <34007133+kuntal-ghosh@users.noreply.github.com>
2019-09-13 16:09:41 +0600

    crwn-clothing added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+import HomePage from "./HomePage/homepage.component";
+
+function App() {
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
